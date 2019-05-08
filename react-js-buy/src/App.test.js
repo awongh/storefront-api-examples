b3170d8c5c633188df61f2c3449b70aa2b3e9ec1
awongh <akira@awongh.com>
2019-05-08 22:24:56 +0800

    sort of working jest with mocks

--- react-js-buy/src/App.test.js
+++ react-js-buy/src/App.test.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {shallow} from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from './App';
+
+// this doesn't work right
+// https://github.com/testing-library/react-testing-library/issues/281
+import { act } from 'react-dom/test-utils';
+
+import checkoutFixture from '../fixtures/checkout-create-new.js';
+//import checkoutFixture from '../js-buy-sdk/fixtures/checkout-create-fixture.js';
+import queryProductsFixture from '../js-buy-sdk/fixtures/query-products-fixture.js';
+import shopInfoFixture from '../js-buy-sdk/fixtures/shop-info-fixture.js';
+
 
 import Client from 'shopify-buy';
 
-jest.mock('shopify-buy');
-
-Client.buildClient.mockImplementation(() => {
-  return {};
-});
-
-/*
 const client = Client.buildClient({
   storefrontAccessToken: 'dd4d4dc146542ba7763305d71d1b3d38',
   domain: 'graphql.myshopify.com'
 });
-*/
+
+beforeEach(() => {
+  console.log("hello");
+  fetch.resetMocks()
+});
+
+afterEach(() => {
+  console.log("bye");
+});
 
 it('renders without crashing', () => {
-  const client = Client.buildClient();
-  const wrapper = shallow(<App client={client}/>);
-  const welcome = <h1>foo and bar</h1>;
-  expect(wrapper).toContainReact(welcome);
+
+  fetch.mockResponseOnce(JSON.stringify( checkoutFixture ), { status: 200, headers: { 'content-type': 'application/json' } });
+
+  fetch.mockResponseOnce(JSON.stringify(queryProductsFixture), { status: 200, headers: { 'content-type': 'application/json' } });
+  fetch.mockResponseOnce(JSON.stringify(shopInfoFixture), { status: 200, headers: { 'content-type': 'application/json' } });
+
+  act(()=>{
+
+    //const wrapper = shallow(<App client={client}/>);
+    const wrapper = mount(<App client={client}/>);
+
+    const welcome = <h1>foo and bar</h1>;
+    expect(wrapper).toContainReact(welcome);
+    expect(global.fetch).toHaveBeenCalledTimes(3);
+  })
+
 });
