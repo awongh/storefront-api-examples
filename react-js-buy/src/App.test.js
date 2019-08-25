609a758c61422c8f0e389ba698c318aeda7ae0fb
awongh <akira@awongh.com>
2019-08-25 14:11:01 +0800

    more tests

--- react-js-buy/src/App.test.js
+++ react-js-buy/src/App.test.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {shallow} from 'enzyme';
+import Enzyme, { shallow, render, mount } from 'enzyme';
 import App from './App';
+import nock from 'nock';
 
 import Client from 'shopify-buy';
 
-jest.mock('shopify-buy');
-
-Client.buildClient.mockImplementation(() => {
-  return {};
-});
-
-/*
 const client = Client.buildClient({
   storefrontAccessToken: 'dd4d4dc146542ba7763305d71d1b3d38',
   domain: 'graphql.myshopify.com'
 });
-*/
 
 it('renders without crashing', () => {
-  const client = Client.buildClient();
-  const wrapper = shallow(<App client={client}/>);
+  const wrapper = mount(<App client={client}/>);
   const welcome = <h1>foo and bar</h1>;
   expect(wrapper).toContainReact(welcome);
 });
